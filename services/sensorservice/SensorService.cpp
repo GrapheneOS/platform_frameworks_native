0844c86cf8e7e8f4db02f4bf597efa9b74f33f5c
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-10-05 19:58:49 +0300

    make a callback on OTHER_SENSORS permission denials
    Change-Id: I67304db0e84ffd201720168108c54cc59177d39d

--- services/sensorservice/SensorService.cpp
+++ services/sensorservice/SensorService.cpp
@@ -2250,6 +2250,8 @@ bool SensorService::canAccessSensor(const Sensor& sensor, const char* operation,
 
     const int32_t opCode = sensor.getRequiredAppOp();
 
+    bool protectedByOtherSensorsPerm = false;
+
     bool canAccess = false;
     if (hasPermissionForSensor(sensor)) {
         // Ensure that the AppOp is allowed, or that there is no necessary app op for the sensor
@@ -2269,6 +2271,7 @@ bool SensorService::canAccessSensor(const Sensor& sensor, const char* operation,
             // upstream allows access to these sensors without the ACTIVITY_RECOGNITION permission
             // for targetSdk < 29 apps, enforce the OTHER_SENSORS permission instead
             const String16 requiredPermission("android.permission.OTHER_SENSORS");
+            protectedByOtherSensorsPerm = true;
 
             // copied from hasPermissionForSensor() below
             canAccess = checkPermission(requiredPermission,
@@ -2279,6 +2282,25 @@ bool SensorService::canAccessSensor(const Sensor& sensor, const char* operation,
     if (!canAccess) {
         ALOGE("%s %s a sensor (%s) without holding %s", String8(opPackageName).c_str(),
               operation, sensor.getName().c_str(), sensor.getRequiredPermission().c_str());
+
+        if (!protectedByOtherSensorsPerm) {
+            protectedByOtherSensorsPerm = (sensor.getRequiredPermission() == "android.permission.OTHER_SENSORS");
+        }
+
+        if (protectedByOtherSensorsPerm) {
+            sp<IBinder> binder = defaultServiceManager()->getService(String16("package_native"));
+            if (binder != nullptr) {
+                sp<content::pm::IPackageManagerNative> packageManager =
+                        interface_cast<content::pm::IPackageManagerNative>(binder);
+                if (packageManager != nullptr) {
+                    int32_t callingUid = IPCThreadState::self()->getCallingUid();
+                    AutoCallerClear acc;
+                    packageManager->onDeniedSpecialRuntimePermissionOp(
+                        String16("android.permission.OTHER_SENSORS"),
+                        callingUid, opPackageName);
+                }
+            }
+        }
     }
 
     return canAccess;
